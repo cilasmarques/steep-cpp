669dcea9ed5e3fd2d7844f5bcd8d80f34a6f7993
cilasmarques <cilas@copin.ufcg.edu.br>
2023-10-09 15:00:09 -0300

    refactor: update execution scripts

--- experimental.sh
+++ experimental.sh
@@ -6,6 +6,7 @@ INPUT_PATH=$3
 OUTPUT_PATH=$4
 SENSOR=$5
 LANDCOVER_DATA_FILE=$6
+THREADS=$7
 
 rm -rf $FINAL_PATH/*
 
@@ -13,7 +14,7 @@ for i in $(seq -f "%02g" 1 30)
 do
 	echo Executing ${i}th experiment
 	mkdir $FINAL_PATH/experimento${i}
-	time sh ./execute.sh $SENSOR $INPUT_PATH $OUTPUT_PATH $METHOD $LANDCOVER_DATA_FILE
+	time sh ./execute.sh $SENSOR $INPUT_PATH $OUTPUT_PATH $METHOD $LANDCOVER_DATA_FILE $THREADS
 	
 	sleep 105
 	mv $OUTPUT_PATH/*.csv $FINAL_PATH/experimento${i}
